bc3d2ce78e9ec91d8b81820dbce0ce4ac3103ab8
mike <mike@mike-g6-2022.(none)>
2012-12-01 16:17:22 -0500

    first urls.py commit

--- ngo_finder/ngo_finder/urls.py
+++ ngo_finder/ngo_finder/urls.py
@@ -14,4 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^ngolist', 'ngo_finder.views.ngolist'),
+    url(r'^ngo/(\d+)', 'ngo_finder.views.ngo'),
 )

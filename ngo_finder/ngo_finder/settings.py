49b85b1122eb18346aa67d17a5e6dc1fbacdd7fe
vito <vito.salerno@gmail.com>
2012-12-01 17:53:42 -0500

    remove print sql details

--- ngo_finder/ngo_finder/settings.py
+++ ngo_finder/ngo_finder/settings.py
@@ -21,9 +21,6 @@ else:
     import dj_database_url
     DATABASES = {}
     DATABASES['default'] =  dj_database_url.config()
-    print DATABASES['default']
-
-
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name

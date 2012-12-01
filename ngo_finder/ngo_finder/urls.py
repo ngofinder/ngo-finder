d9b85f8be2867cbb4307d94336fa25218067d79f
vito <vito.salerno@gmail.com>
2012-12-01 16:02:03 -0500

    hello world route to test heroku

--- ngo_finder/ngo_finder/urls.py
+++ ngo_finder/ngo_finder/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'ngo_finder.views.home', name='home'),
+    url(r'^$', 'ngo_finder.views.hello_world', name='home'),
     # url(r'^ngo_finder/', include('ngo_finder.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:

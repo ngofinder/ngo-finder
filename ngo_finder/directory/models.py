61506179c2e7f45950421b54a3f7a187b65c450c
mike <mike@mike-g6-2022.(none)>
2012-12-01 19:46:01 -0500

    model set null=True

--- ngo_finder/directory/models.py
+++ ngo_finder/directory/models.py
@@ -9,31 +9,31 @@ class Sector(models.Model):
 
 class Organization(models.Model):
     name = models.CharField(max_length=255)
-    geo_scope = models.ForeignKey(Geo_scope)
+    geo_scope = models.ForeignKey(Geo_scope,null=True)
     sectors = models.ManyToManyField(Sector)
 
 class Organization_location(models.Model):
     organization = models.ForeignKey(Organization)
-    primary = models.BooleanField() # record is org's main location
-    country = models.CharField(max_length=255)
-    state = models.CharField(max_length=255)
-    county = models.CharField(max_length=255)
-    city = models.CharField(max_length=255)
-    street = models.CharField(max_length=255)
-    postalcode = models.CharField(max_length=255)
+    primary = models.BooleanField(default=False) # record is org's main location
+    country = models.CharField(max_length=255,null=True)
+    state = models.CharField(max_length=255,null=True)
+    county = models.CharField(max_length=255,null=True)
+    city = models.CharField(max_length=255,null=True)
+    street = models.CharField(max_length=255,null=True)
+    postalcode = models.CharField(max_length=255,null=True)
 
 class Organization_contact(models.Model):
     organization = models.ForeignKey(Organization)
-    name = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255)
-    primary_phone = models.CharField(max_length=255)
-    secondary_phone = models.CharField(max_length=255)
+    name = models.CharField(max_length=255,null=True)
+    email = models.EmailField(max_length=255,null=True)
+    primary_phone = models.CharField(max_length=255,null=True)
+    secondary_phone = models.CharField(max_length=255,null=True)
 
 class Organization_comment(models.Model):
     organization = models.ForeignKey(Organization)
     user = models.ForeignKey(User)
-    subject = models.CharField(max_length=255)
-    text = models.TextField()
+    subject = models.CharField(max_length=255,null=True)
+    text = models.TextField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 

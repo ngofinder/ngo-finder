6b0f40978a22284da6bce1459be9bcb80f181234
vito <vito.salerno@gmail.com>
2012-12-02 10:30:38 -0500

    comment search

--- ngo_finder/directory/search_indexes.py
+++ ngo_finder/directory/search_indexes.py
@@ -1,9 +1,13 @@
 from haystack.indexes import *
 from haystack import site
-from models import Organization
+from models import Organization, Comment
 
 class OrganizationIndex(SearchIndex):
 	text = CharField(document=True, use_template=True)
 	sector = CharField(model_attr='sector')
 
+class CommentIndex(SearchIndex):
+	text = CharField(document=True, use_template=True)
+
 site.register(Organization, OrganizationIndex)	
+site.register(Comment, CommentIndex)

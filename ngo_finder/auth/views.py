21dd92e4b095e8c63531baf28986761fe86c6531
Bob Lannon <blannon@gmail.com>
2012-12-02 14:25:39 -0500

    fixing auth

--- ngo_finder/auth/views.py
+++ ngo_finder/auth/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response,redirect
 from django.contrib.auth import authenticate, login
 from django.core.context_processors import csrf
 from django.template import RequestContext
@@ -6,23 +6,32 @@ from django.template import RequestContext
 def login_user(request):
     state = "Please log in below..."
     username = password = ''
+    signed_in = False
     if request.POST:
+        if request.POST.get('signed_in'):
+            c = RequestContext(request,{})
+            c.update(csrf(request))
+            return redirect('/',c)
         username = request.POST.get('username')
-        username = request.POST.get('password')
+        password = request.POST.get('password')
 
         user = authenticate(username=username,password=password)
         if user is not None:
             if user.is_active:
                 login(request,user)
                 state = "You're successfully logged in"
+                signed_in = True
             else:
                 state = "Your account is not active, please contact the site admin"
+                signed_in = False
         else:
             state = "Your username and/or password were incorrect"
+            signed_in = False
 
     c = RequestContext(request, { 
                                     'state'    : state,
-                                    'username' : username  })
+                                    'username' : username,
+                                    'signed_in': signed_in})
     c.update(csrf(request))
 
     return render_to_response('auth/signin.html',c)

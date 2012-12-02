8439a33d3cfd9dae27ff1bbe32907138bad02462
vito <vito.salerno@gmail.com>
2012-12-02 14:01:28 -0500

    add organization profile template

--- ngo_finder/directory/views.py
+++ ngo_finder/directory/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from directory.models import Organization
 
 def hello_world(request):
-	return render_to_response("directory/hello_world.html")
+        return render_to_response("directory/hello_world.html")
+
+def ngo(request, ngo_id):
+        organization = Organization.objects.filter(id = ngo_id)
+        return render_to_response("directory/profile.html", {"organization" :organization})
